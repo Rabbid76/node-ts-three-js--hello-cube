36595ce3fcceab272b8a81bba2c93436f2d90d5d
Gernot Steinegger <gernot.steinegger@roomle.com>
2023-04-11 07:07:35 +0200

    refactor: import three

--- src/client/threeClient.ts
+++ src/client/threeClient.ts
@@ -1,4 +1,21 @@
-import * as THREE from 'three';
+import {
+    ACESFilmicToneMapping,
+    AxesHelper,
+    BoxGeometry,
+    Color,
+    DirectionalLight,
+    GridHelper,
+    LinearEncoding,
+    Mesh,
+    MeshPhysicalMaterial,
+    PerspectiveCamera,
+    PCFSoftShadowMap,
+    PlaneGeometry,
+    PMREMGenerator,
+    Scene,
+    ShadowMaterial,
+    WebGLRenderer,
+} from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { TransformControls } from 'three/examples/jsm/controls/TransformControls.js';
 import { RoomEnvironment } from 'three/examples/jsm/environments/RoomEnvironment.js';
@@ -7,36 +24,36 @@ import Stats from 'three/examples/jsm/libs/stats.module'
 import { GUI } from 'dat.gui'
 
 export const helloCube = (canvas: any) => {
-    const renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true, alpha: true});
+    const renderer = new WebGLRenderer({canvas: canvas, antialias: true, alpha: true});
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
     renderer.shadowMap.enabled = true;
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-    renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.shadowMap.type = PCFSoftShadowMap;
+    renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.outputEncoding = THREE.LinearEncoding;
-    //renderer.toneMapping = THREE.NoToneMapping;
-    //renderer.outputEncoding = THREE.sRGBEncoding;
-    renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    renderer.outputEncoding = LinearEncoding;
+    //renderer.toneMapping = NoToneMapping;
+    //renderer.outputEncoding = sRGBEncoding;
+    renderer.toneMapping = ACESFilmicToneMapping;
 
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.y = 4;
     camera.position.z = 8;
-    const controls = new OrbitControls(camera, renderer.domElement)
+    const controls = new OrbitControls(camera, renderer.domElement);
 
-    const scene = new THREE.Scene();
-    scene.background = new THREE.Color(0xc0c0c0);
-    const pmremGenerator = new THREE.PMREMGenerator(renderer);
+    const scene = new Scene();
+    scene.background = new Color(0xc0c0c0);
+    const pmremGenerator = new PMREMGenerator(renderer);
     const environmentTexture = pmremGenerator.fromScene(new RoomEnvironment(), 0.04).texture;
     scene.environment = environmentTexture;
     scene.background = environmentTexture;
 
-    const gridHelper = new THREE.GridHelper(10, 10);
+    const gridHelper = new GridHelper(10, 10);
     scene.add(gridHelper);
-    const axesHelper = new THREE.AxesHelper(2);
+    const axesHelper = new AxesHelper(2);
     scene.add(axesHelper);
 
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+    const directionalLight = new DirectionalLight(0xffffff, 0.5);
     directionalLight.position.set(1, 3, 1);
     directionalLight.castShadow = true;
     scene.add(directionalLight);
@@ -48,19 +65,19 @@ export const helloCube = (canvas: any) => {
     lightTransformControl.visible = false;
     scene.add(lightTransformControl);
     
-    const groundGeometry = new THREE.PlaneGeometry(10, 10);
+    const groundGeometry = new PlaneGeometry(10, 10);
     groundGeometry.rotateX(-Math.PI / 2);
-    const groundMaterial = new THREE.ShadowMaterial();
-    const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
+    const groundMaterial = new ShadowMaterial();
+    const groundMesh = new Mesh(groundGeometry, groundMaterial);
     groundMesh.receiveShadow = true;
     scene.add(groundMesh);
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshPhysicalMaterial({color: 0xe02020});
-    const mesh = new THREE.Mesh(geometry, material);
+    const geometry = new BoxGeometry(1, 1, 1);
+    const material = new MeshPhysicalMaterial({color: 0xe02020});
+    const mesh = new Mesh(geometry, material);
     mesh.castShadow = true;
     mesh.receiveShadow = true;
-    mesh.position.y = 0.5
+    mesh.position.y = 0.5;
     scene.add(mesh);
     const meshTransformControl = new TransformControls(camera, renderer.domElement);
     meshTransformControl.addEventListener( 'dragging-changed', (event: any) => {
@@ -76,7 +93,7 @@ export const helloCube = (canvas: any) => {
     const gui = new GUI();
     const uiProperties = {
         'mesh transform control': meshTransformControl.visible,
-        'light transform control': lightTransformControl.visible
+        'light transform control': lightTransformControl.visible,
     }
     gui.add(uiProperties, 'mesh transform control').onChange((value) => meshTransformControl.visible = value);
     gui.add(uiProperties, 'light transform control').onChange((value) => lightTransformControl.visible = value);
